326a8ffddcffe31a5e5ba8a411916dbc1e3f5554
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-03-12 21:01:09 -0500

    Desarrollo de componentes para los formularios de app

---
+++ meeti-app/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Form from "@/src/shared/components/forms/Form";
+
+export default function LoginForm() {
+  return (
+    <Form>
+      <label className="block" htmlFor="email">
+        E-mail
+      </label>
+      <input
+        type="email"
+        id="email"
+        placeholder="Ingresa tu E-mail"
+        className="border-slate-200 w-full p-2"
+      />
+      <label className="block" htmlFor="password">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        placeholder="Ingresa tu Password"
+        className="border-slate-200 w-full p-2"
+      />
+    </Form>
+  );
+}
