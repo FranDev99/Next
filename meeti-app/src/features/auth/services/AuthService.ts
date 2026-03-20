e99d8de2c5b07b5dae998c82d90a5eb80fbe26dc
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-03-20 13:06:29 -0500

    Verificación de existencia de cuenta del usuario en la base de datos

--- meeti-app/src/features/auth/services/AuthService.ts
+++ meeti-app/src/features/auth/services/AuthService.ts
@@ -1,9 +1,24 @@
 import { auth } from "@/lib/auth";
 import { SignUpInput } from "../schemas/authSchema";
+import { authRepository, IAuthRepository } from "./AuthRepository";
 
 class AuthService {
+
+    constructor(
+        private authRepository: IAuthRepository
+    ) { }
+
     async register(credentials: SignUpInput) {
         const { name, email, password } = credentials
+
+        //Revisar si el usuario existe
+        const user = await this.authRepository.userExists(email)
+        if (user) {
+            return {
+                error: 'Este e-mail ya esta registrado',
+                success: ''
+            }
+        }
 
         // Manejar el registro de usuario
         await auth.api.signUpEmail({
@@ -21,4 +36,4 @@ class AuthService {
     }
 }
 
-export const authService = new AuthService();
+export const authService = new AuthService(authRepository);
